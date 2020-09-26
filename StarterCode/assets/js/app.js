14d24254464dcf9e142c9683d13403c2bdedc6cd
KESHIA VEREDA <vereda761@gmail.com>
2020-09-26 10:38:29 -0700

    wrapper

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -10,4 +10,13 @@ var margin = {
 
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
-// 
+
+var svg = d3
+  .select("#scatter")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
